b43f512e11bec5df1ea621faee7658e8ac0bff56
YuriCardosoBalieiro <yuri.c.balieiro@gmail.com>
2026-06-06 23:52:15 -0300

    feat(application): implement list available slots use case

--- src/domain/interfaces/IAvailabilityRepository.ts
+++ src/domain/interfaces/IAvailabilityRepository.ts
@@ -2,4 +2,5 @@ import { Availability } from '../entities/Availability';
 
 export interface IAvailabilityRepository {
   save(availability: Availability): Promise<Availability>;
+  findByProviderId(providerId: string): Promise<Availability | null>;
 }
